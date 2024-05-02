bf480c5d1e68a8c15865dbcbc20a6edfe8218ae4
Keoni Mortensen <keonimortensen@gmail.com>
2024-05-02 15:50:58 -0700

    scraping scores example

--- main.py
+++ main.py
@@ -8,14 +8,6 @@ import pandas as pd
 import time
 import random
 
-# notebooks (google colab / jupyter notebook)
-#   - They are nice becuase you can truly run code step by step instead of having to run the whole thing over and over again.
-
-# Prepare to post on github and use this to create a dataset that can be posted on kaggle.
-
-# Should I have this connect to google bigquery?
-# ^^^ I think this will be the next thing.
-
 """
 GLOBAL VARIABLES:
 path_to_webdriver - string - absolute path to webdriver on personal machine.
@@ -32,31 +24,22 @@ def main():
     # Display all available seasons and weeks
     available_options = scraper.display_seasons_and_weeks()
 
-    # # Preparing a loop
-    # available_years = available_options["Season"].tolist()
-    # available_weeks = available_options["Weeks"].tolist()
-    # print(available_years)
-    # # print(available_weeks)
+    # Preparing a loop
+    available_years = available_options["Season"].tolist()
+    available_weeks = available_options["Weeks"].tolist()
 
-    # # Simulate normal user on website
-    # base_sleep = 5
-    # random_additional_sleep = 5
+    # Simulate normal user on website
+    base_sleep = 5
+    random_additional_sleep = 5
 
-    # for i in range(2,len(available_years),1):
-    #     for j in available_weeks[i]:
-    #         print(available_years[i] + " " + j)
-    #         adding_data = scraper.get_game_week_data(available_years[i], j)
-    #         scraper.data = pd.concat([scraper.data, adding_data], ignore_index=True)
-    #         time.sleep(base_sleep + random.random() * random_additional_sleep)
-    #     scraper.data.to_csv('{}_scores.csv'.format(available_years[i]), index=False)
-    #     scraper.data = pd.DataFrame()
-
-    # print(scraper.get_game_week_data("2017", "Preseason Week 4"))
-    # print(scraper.get_game_week_data("2022", "Week 17"))
-    # print(scraper.get_game_week_data("2022", "Week 11"))
-    # print(scraper.get_game_week_data("2017", "Pro Bowl"))
-    # print(scraper.get_game_week_data("2023", "Week 6"))
-    # print(scraper.get_game_week_data("2023", "Super Bowl"))
+    for i in range(0,len(available_years)-6,1):
+        for j in available_weeks[i]:
+            print(available_years[i] + " " + j)
+            adding_data = scraper.get_game_week_data(available_years[i], j)
+            scraper.data = pd.concat([scraper.data, adding_data], ignore_index=True)
+            time.sleep(base_sleep + random.random() * random_additional_sleep)
+        scraper.data.to_csv('{}_scores.csv'.format(available_years[i]), index=False)
+        scraper.data = pd.DataFrame()
     
     scraper.close_driver()
     
