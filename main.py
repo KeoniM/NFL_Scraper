0f88d6b8578f2b8dc065fcb268515524a8dd65e0
Keoni Mortensen <keonimortensen@gmail.com>
2025-08-07 16:50:33 -0700

    updated data to Season 2025 Hall Of Fame

--- main.py
+++ main.py
@@ -94,9 +94,9 @@ def main():
     # Example 1 (get_scores_given_seasons_and_weeks)
     # - Get scores from a given list of seasons and their associated weeks
     #   - Provide a list of file names for csv files, if 'None' then default will be used
-    example_seasons = ['2024']
+    example_seasons = ['2025']
     # example_weeks = [['Week 18', 'Wild Card']]
-    example_weeks = [all_available_weeks_for_associated_seasons[all_available_seasons.index("2024")]]
+    example_weeks = [all_available_weeks_for_associated_seasons[all_available_seasons.index("2025")]]
     example_file_names = None
     get_scores_given_seasons_and_weeks(scraper, 
                                        example_seasons, 
@@ -117,8 +117,8 @@ def main():
 
     # Example 1 (get_plays_given_seasons_and_weeks)
     # - Grabbing plays from specified list of seasons and weeks
-    example_seasons = ['2024']
-    example_weeks = [['Super Bowl']]
+    example_seasons = ['2025']
+    example_weeks = [['Hall Of Fame']]
     # example_file_names = ['2024_week 3_plays']
     example_file_names = None
     get_plays_given_seasons_and_weeks(scraper, example_seasons, example_weeks, example_file_names)
