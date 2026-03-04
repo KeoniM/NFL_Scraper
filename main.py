3f6bd91c386243abba9f09a5bcf587032f2ce4bd
Keoni Mortensen <keonimortensen@gmail.com>
2026-03-03 17:45:22 -0800

    Rescraping updated data

--- main.py
+++ main.py
@@ -122,14 +122,14 @@ def main():
     # Example 2 (get_plays_given_seasons_and_weeks)
     # Grabbing all plays from a specified season
     # - Without start week given
-    index_of_desired_season = all_available_seasons.index("2018")
+    index_of_desired_season = all_available_seasons.index("2019")
     example_season = [all_available_seasons[index_of_desired_season]] # Needs to be a list of lists
     example_weeks = all_available_weeks_for_associated_seasons[index_of_desired_season]
-    # get_plays_given_seasons_and_weeks(scraper, example_season, [example_weeks])
+    get_plays_given_seasons_and_weeks(scraper, example_season, [example_weeks])
     # # - With start week given
-    example_start_week = example_weeks.index("WEEK 12")
-    example_week_start = [example_weeks[example_start_week::]] # Needs to be a list of lists
-    get_plays_given_seasons_and_weeks(scraper, example_season, example_week_start)
+    # example_start_week = example_weeks.index("WEEK 12")
+    # example_week_start = [example_weeks[example_start_week::]] # Needs to be a list of lists
+    # get_plays_given_seasons_and_weeks(scraper, example_season, example_week_start)
 
     # # Example 3 (get_plays_given_seasons_and_weeks)
     # # - Grabbing all plays from every season available 
